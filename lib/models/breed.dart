b6e7d36db69360d9a931fd51f4e659d25ea0a1f6
Sandip <sandipramanik07@gmail.com>
2021-12-13 23:24:47 +0530

    Add dog and breed models

---
+++ lib/models/breed.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+class Breed {
+  final int id;
+  final String name;
+  final String description;
+
+  Breed({
+    required this.id,
+    required this.name,
+    required this.description,
+  });
+
+  // Convert a Breed into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+    };
+  }
+
+  factory Breed.fromMap(Map<String, dynamic> map) {
+    return Breed(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      description: map['description'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Breed.fromJson(String source) => Breed.fromMap(json.decode(source));
+
+  // Implement toString to make it easier to see information about
+  // each breed when using the print statement.
+  @override
+  String toString() => 'Breed(id: $id, name: $name, description: $description)';
+}
---
+++ lib/models/dog.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+
+class Dog {
+  final int id;
+  final String name;
+  final int age;
+  final Color color;
+  final int breedId;
+
+  Dog({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.color,
+    required this.breedId,
+  });
+
+  // Convert a Dog into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'color': color.value,
+      'breedId': breedId,
+    };
+  }
+
+  factory Dog.fromMap(Map<String, dynamic> map) {
+    return Dog(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      age: map['age']?.toInt() ?? 0,
+      color: Color(map['color']),
+      breedId: map['breedId']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Dog.fromJson(String source) => Dog.fromMap(json.decode(source));
+
+  // Implement toString to make it easier to see information about
+  // each dog when using the print statement.
+  @override
+  String toString() {
+    return 'Dog(id: $id, name: $name, age: $age, color: $color, breedId: $breedId)';
+  }
+}
